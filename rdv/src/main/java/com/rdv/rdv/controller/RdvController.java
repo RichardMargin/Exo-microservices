36193a0f6a6ec59fdc7eabc9dc555da2fd02caff
rehioui walid <walid.rehioui@telecomnancy.eu>
2023-01-20 15:48:24 +0100

    ajout controller pour le microservice rdv

---
+++ rdv/src/main/java/com/rdv/rdv/controller/RdvController.java
@@ -0,0 +1,76 @@
+package com.rdv.rdv.controller;
+
+import com.rdv.rdv.model.Rdv;
+import com.rdv.rdv.service.RdvService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/rdvs")
+public class RdvController {
+
+    @Autowired
+    private RdvService rdvService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Rdv>> findAll() {
+        List<Rdv> rdvs = rdvService.findAll();
+        if (rdvs != null && !rdvs.isEmpty()) {
+            return new ResponseEntity<>(rdvs, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Rdv> findById(@PathVariable Long id) {
+        Optional<Rdv> rdv = rdvService.findById(id);
+        if (rdv.isPresent()) {
+            return new ResponseEntity<>(rdv.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/idPatient/{idPatient}/idMedecin/{idMedecin}")
+    public ResponseEntity<Rdv> save(@RequestBody Rdv rdv, @PathVariable Long idPatient, @PathVariable Long idMedecin) {
+        Rdv rd = rdvService.save(rdv, idPatient, idMedecin);
+        if (rd != null) {
+            return new ResponseEntity<>(rd, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @PutMapping("/idPatient/{idPatient}/idMedecin/{idMedecin}")
+    public ResponseEntity<Rdv> update(@RequestBody Rdv rdv, @PathVariable Long idPatient, @PathVariable Long idMedecin) {
+        Rdv rd = rdvService.save(rdv, idPatient, idMedecin);
+        if (rd != null) {
+            return new ResponseEntity<>(rd, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Rdv> deleteById(@PathVariable Long id) {
+        rdvService.deleteById(id);
+        Optional<Rdv> rdv = rdvService.findById(id);
+        if (!rdv.isPresent()) {
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity<Rdv> deleteAll() {
+        rdvService.deleteAll();
+        List<Rdv> rdvs = rdvService.findAll();
+        if (rdvs == null || rdvs.isEmpty()) {
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
